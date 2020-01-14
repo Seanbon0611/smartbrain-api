c5a18a52835ae2919476768c58ae1998321d43da
Sean Dever <Seand0611@gmail.com>
2020-01-14 11:10:07 -0800

    created server for face recognition app

---
+++ controllers/image.js
@@ -0,0 +1,32 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: 'c5b6e6ddb95747738351d7c77744b1a3'
+  });
+const handleApiCall = (req, res) => {
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('unable to work withh API'))
+}
+
+const handleImage = (db) => (req, res) => {
+    const { id } = req.body;
+    db('users')
+    .where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('Unable to retrieve entries'))
+  }
+
+  
+
+module.exports = {
+    handleImage: handleImage,
+    handleApiCall: handleApiCall
+};
